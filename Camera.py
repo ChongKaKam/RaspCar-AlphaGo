0ee9fb0fe3f81227889c70c660cd15ee5b12122a
Chris <1020158376@qq.com>
2022-05-14 16:31:51 +0100

    Complete CameraModule

--- Camera.py
+++ Camera.py
@@ -1,6 +1,17 @@
 import cv2
-from Servo import Servo
+from Servo import Servo_Manager
 
-class Camera:
 
-    pass
+class CameraModule:
+    def __init__(self, device=0) -> None:
+        self.device = cv2.VideoCapture(device)
+        if not self.device.isOpened(): 
+            print('Cannot open camera')
+            return None
+        self.Servos = Servo_Manager()
+        self.Servos.DeviceInit()
+    
+
+if __name__=="__main__":
+    camera = CameraModule()
+    
